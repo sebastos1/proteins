c1935254e607299da0f0661a1c42e1a0e161c6ad
seb <sebastiandalknu@gmail.com>
2022-11-16 22:04:43 +0100

    bug fix

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,122 @@
-use serde_json::Value;
+use http::Uri;
+use sailfish::TemplateOnce;
 use std::collections::HashMap;
+use std::sync::Once;
+use std::thread;
+use warp::reply::html;
+use warp::Filter;
 
-fn main() {
-    let url = std::fs::read_to_string("link.txt").unwrap();
+mod update;
+use update::update;
 
-    let res = reqwest::blocking::get(url).unwrap()
-        .text().unwrap();
-    let map: HashMap<String, Value> = serde_json::from_str(&res).unwrap();
+#[derive(TemplateOnce)]
+#[template(path = "index.html")]
+struct Index {
+    foods: HashMap<String, HashMap<String, String>>,
+    sort: Vec<String>,
+}
 
-    let mut foods = HashMap::new();
-    for x in 0..map["foods"].as_array().unwrap().len() {
-        let mut nutrients = HashMap::new();
+#[derive(TemplateOnce)]
+#[template(path = "more.html")]
+struct More<'a> {
+    order: Vec<&'a str>,
+    product: String,
+    foods: HashMap<String, HashMap<String, String>>,
+}
 
-        macro_rules! insert {
-            ($a:expr, $b:expr) => {
-                nutrients.insert(
-                    $a,
-                    map["foods"][x][$b]["value"].as_str().unwrap()
-                );
-            }
+#[tokio::main]
+async fn main() {
+    let static_assets = warp::path("static").and(warp::fs::dir("static/"));
+    let foods: HashMap<String, HashMap<String, String>> =
+        serde_json::from_str(&std::fs::read_to_string("output.json").unwrap()).unwrap();
+    let mut sort: Vec<String> = foods.clone().into_keys().collect();
+    sort.sort_by_key(|name| name.to_lowercase());
+
+    let index = warp::path!().map({
+        let foods = foods.clone();
+        let sort = sort.clone();
+        move || {
+            let foods = foods.clone();
+            let sort = sort.clone();
+            html(Index { foods, sort }.render_once().unwrap())
         }
+    });
 
-        insert!("kJ", "Energi1");
-        insert!("kcal", "Energi2");
-        insert!("Protein", "Protein");
-        insert!("Karbohydrater", "Karbo");
-        insert!("Fett", "Fett");
-        insert!("Fiber", "Fiber");
+    let search = warp::path!("search")
+        .and(warp::query::<Vec<(String, String)>>())
+        .map({
+            let foods = foods.clone();
+            let sort = sort.clone();
+            move |form: Vec<(String, String)>| {
+                let foods = foods.clone();
+                let alphabetical = sort.clone();
+                let mut sort: Vec<String> = Vec::new();
+                for item in alphabetical {
+                    if item.to_lowercase().contains(&form[0].1.to_lowercase()) {
+                        sort.push(item)
+                    }
+                }
+                html(Index { foods, sort }.render_once().unwrap())
+            }
+        });
 
-        foods.insert(
-            map["foods"][x]["name"].as_str().unwrap(),
-            nutrients
-        );
-    }
-    println!("{:#?}", foods);
+    let update = warp::path("update").map({
+        move || {
+            static START: Once = Once::new();
+            thread::spawn(|| {
+                START.call_once(|| {
+                    update();
+                });
+            })
+            .join()
+            .unwrap();
+            warp::redirect(Uri::from_static("/"))
+        }
+    });
+
+    // auto format moment
+    let order = vec![
+        "Vann",
+        "Hvorav mettet",
+        "Hvorav enumettet",
+        "Hvorav flerumettet",
+        "Tilsatt sukker",
+        "Transfett",
+        "Fiber",
+        "Kolesterol",
+        "Stivelse",
+        "Salt",
+        "Omega-3",
+        "Alkohol",
+        "Vit A",
+        "Vit B1",
+        "Vit B2",
+        "Vit B6",
+        "Vit B12",
+        "Vit C",
+        "Vit D",
+        "Vit E",
+    ];
+
+    let product = warp::path("product").and(warp::path::param()).map({
+        let order = order.clone();
+        let foods = foods.clone();
+        move |product: String| {
+            let order = order.clone();
+            let product = urlencoding::decode(&product).unwrap().to_string();
+            let foods = foods.clone();
+            html(
+                More {
+                    order,
+                    product,
+                    foods,
+                }
+                .render_once()
+                .unwrap(),
+            )
+        }
+    });
+
+    let routes = index.or(static_assets).or(product).or(update).or(search);
+    warp::serve(routes).run(([0, 0, 0, 0], 8000)).await;
 }
---
+++ src/update.rs
@@ -0,0 +1,68 @@
+use serde_json::Value;
+use std::collections::HashMap;
+
+pub fn update() {
+    let url = std::fs::read_to_string("link.txt").unwrap();
+    let res = reqwest::blocking::get(url).unwrap().text().unwrap();
+    let map: HashMap<String, Value> = serde_json::from_str(&res).unwrap();
+    let mut foods = HashMap::new();
+
+    for x in 0..map["foods"].as_array().unwrap().len() {
+        let mut nutrients = HashMap::new();
+
+        macro_rules! insert {
+            ($a:expr, $b:expr) => {
+                let string = map["foods"][x][$b]["value"].as_str().unwrap();
+                if string != "0" {
+                    Some(nutrients.insert($a, string))
+                } else {
+                    None
+                }
+            };
+        }
+
+        macro_rules! floor {
+            ($a:expr, $b:expr) => {
+                let mut string = map["foods"][x][$b]["value"].as_str().unwrap();
+                match string.find(".") {
+                    Some(x) => string = &string[0..x],
+                    None => (),
+                };
+                nutrients.insert($a, string)
+            };
+        }
+
+        floor!("kJ", "Energi1");
+        floor!("kcal", "Energi2");
+        insert!("Protein", "Protein");
+        insert!("Karbohydrater", "Karbo");
+        insert!("Fett", "Fett");
+        insert!("Fiber", "Fiber");
+        insert!("Omega-3", "Omega-3");
+        insert!("Hvorav mettet", "Mettet");
+        insert!("Hvorav enumettet", "Enumet");
+        insert!("Hvorav flerumettet", "Flerum");
+        insert!("Tilsatt sukker", "Sukker");
+        insert!("Transfett", "Trans");
+        insert!("Kolesterol", "Kolest");
+        insert!("Stivelse", "Stivel");
+        insert!("Salt", "NaCl");
+        insert!("Alkohol", "Alko");
+        insert!("Vit A", "Vit A");
+        insert!("Vit B1", "Vit B1");
+        insert!("Vit B2", "Vit B2");
+        insert!("Vit B6", "Vit B6");
+        insert!("Vit B12", "Vit B12");
+        insert!("Vit C", "Vit C");
+        insert!("Vit D", "Vit D");
+        insert!("Vit E", "Vit E");
+        insert!("Vann", "Vann");
+
+        println!("lolololol");
+
+        foods.insert(map["foods"][x]["name"].as_str().unwrap(), nutrients);
+    }
+
+    let file = std::fs::File::create("output.json").unwrap();
+    serde_json::to_writer(file, &foods).unwrap();
+}
